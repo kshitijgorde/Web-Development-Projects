9cea3b634120d74c0b78eda69d78bb190a90c535
Kshitij G <kshitijgorde@rocketmail.com>
2018-02-15 18:46:23 -0500

    host

--- stockanalysis/settings.py
+++ stockanalysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm1wrv4c1ove4_t+q9g=wx6!^*g@4@_#*(x_98s5w7k=u7o1a_i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://capitech.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['capitech.herokuapp.com', 'localhost']
 
 
 # Application definition
