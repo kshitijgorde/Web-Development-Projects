efd2344df79adea122281611482456aa2fb830c9
Kshitij G <kshitijgorde@rocketmail.com>
2018-02-16 00:01:03 -0500

    removed db

--- stockanalysis/stockanalysis/settings.py
+++ stockanalysis/stockanalysis/settings.py
@@ -116,16 +116,16 @@ WSGI_APPLICATION = 'stockanalysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stocks_portfolio',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'stocks_portfolio',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
