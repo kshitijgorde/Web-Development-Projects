ded5b692c45725128801d91f0b35993e0bdd4f4b
Kshitij G <kshitijgorde@rocketmail.com>
2018-02-16 18:08:56 -0500

    should work

--- stockanalysis/manage.py
+++ stockanalysis/manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    path = '/home/kshitijgorde/Documents/My_Projects/Web-Development-Projects/stockanalysis/'
+    if path not in sys.path:
+        sys.path.append(path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stockanalysis.settings")
     try:
         from django.core.management import execute_from_command_line
